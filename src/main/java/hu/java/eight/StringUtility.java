f74c3e5f78350c8cd78e2eb3dab5c43b2cc21cc4
Leonard Laszlo <laslaul@yahoo.com>
2017-03-16 21:22:42 +0100

    Add multi-thread filtering in Java 8 example

--- src/main/java/hu/java/eight/StringUtility.java
+++ src/main/java/hu/java/eight/StringUtility.java
@@ -3,12 +3,21 @@ package hu.java.eight;
 import java.util.List;
 
 import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
 
 public class StringUtility {
+
+    // Pass method as parameter in Java 8
     public List<String> sortStringsByLength(List<String> list) {
-        // Pass method as parameter (Java 8)
         list.sort(comparing(String::length));
 
         return list;
+    }
+
+    // Multi-thread filtering in Java 8
+    public List<String> filterHigherThan(int lowerLengthBound, List<String> input) {
+        return input.parallelStream()
+                .filter((String word) -> word.length() > lowerLengthBound)
+                .collect(toList());
     }
 }

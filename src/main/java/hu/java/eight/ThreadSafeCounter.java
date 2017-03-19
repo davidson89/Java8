d842ad3d7880931a42a971e5dd0221b907c85ce9
Leonard Laszlo <laslaul@yahoo.com>
2017-03-19 12:47:24 +0100

    Add thread safe counter example

---
+++ src/main/java/hu/java/eight/ThreadSafeCounter.java
@@ -0,0 +1,22 @@
+package hu.java.eight;
+
+public class ThreadSafeCounter {
+    private Object lock;
+
+    private int count;
+
+    public ThreadSafeCounter() {
+        this.lock = new Object();
+        this.count = 0;
+    }
+
+    public void increment() {
+        synchronized (lock) {
+            count++;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
---
+++ src/test/java/hu/java/eight/ThreadSafeCounterTest.java
@@ -0,0 +1,38 @@
+package hu.java.eight;
+
+import org.testng.annotations.Test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.testng.Assert.assertEquals;
+
+public class ThreadSafeCounterTest {
+
+    private static final Logger LOGGER = Logger.getLogger( ThreadSafeCounterTest.class.getName() );
+    public static final Level LOG_LEVEL = Level.FINE;
+    public static final String CURRENT_THREAD_ID_MESSAGE_TEMPLATE = "Current thread id: {0}";
+
+    private ThreadSafeCounter underTest;
+
+    public ThreadSafeCounterTest() {
+        underTest = new ThreadSafeCounter();
+    }
+
+    @Test
+    public void shouldExecuteThreadSafeOperation() throws InterruptedException {
+        LOGGER.log( LOG_LEVEL, CURRENT_THREAD_ID_MESSAGE_TEMPLATE, Thread.currentThread().getId() );
+
+        underTest.increment();
+
+        Thread t = new Thread(() -> {
+            LOGGER.log( LOG_LEVEL, CURRENT_THREAD_ID_MESSAGE_TEMPLATE, Thread.currentThread().getId() );
+            underTest.increment();
+        });
+
+        t.start();
+        t.join();
+
+        assertEquals(underTest.getCount(), 2, "Counter value should be 2");
+    }
+}
